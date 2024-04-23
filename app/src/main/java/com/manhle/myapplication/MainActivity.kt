ebc9aee7c7feafd6e6167d17200e7dfedb68dcb5
Le Tran Huu Manh <manhlth.22it@vku.udn.vn>
2024-04-23 14:24:13 +0700

    Add color

--- app/src/main/java/com/manhle/myapplication/MainActivity.kt
+++ app/src/main/java/com/manhle/myapplication/MainActivity.kt
@@ -5,15 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.manhle.myapplication.nav.OrderFoodNavavigation
 import com.manhle.myapplication.screens.Login
+import com.manhle.myapplication.screens.Menu
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Login(
-// Tran Xuan Duc test
-                navigateToSignUp = { }
-            )
+            Menu()
         }
     }
 }
